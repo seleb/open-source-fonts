5fe9e5b19a197f7ee91e35c4eb0e765b9e8866fe
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-10-16 23:20:47 -0400

    bake errors additively + avoid overwriting reason with "manually excluded"

--- bakeErrors.js
+++ bakeErrors.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
 const errors = require('./output/_errors.json');
-fs.writeFileSync('./badFonts.env', Array.from(new Set(errors.map(i => `${i.font} # ${typeof i.error === 'string' ? i.error : JSON.stringify(i.error)}`.trim()))).join('\n'));
+let badFonts = fs.readFileSync('./badFonts.env', { encoding: 'utf-8' }).trim();
+badFonts = badFonts
+	.split('\n')
+	.map(i => {
+		const [font, ...err] = i.split('#');
+		return { font: font.trim(), error: err.join('#').trim() };
+	})
+	.concat(errors.filter(i => i.error !== 'manually excluded'));
+
+fs.writeFileSync('./badFonts.env', Array.from(new Set(badFonts.map(i => `${i.font} # ${typeof i.error === 'string' ? i.error : JSON.stringify(i.error)}`.trim()))).join('\n'), { encoding: 'utf-8' });
